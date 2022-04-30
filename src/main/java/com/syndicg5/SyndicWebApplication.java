5a9472c6a39f86c16db3b35c8428cae14b99dce8
YassineMest <yassinemestaoui98@gmail.com>
2022-04-30 13:18:44 +0000

    started project

---
+++ src/main/java/com/syndicg5/SyndicWebApplication.java
@@ -0,0 +1,13 @@
+package com.syndicg5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyndicWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyndicWebApplication.class, args);
+    }
+
+}
