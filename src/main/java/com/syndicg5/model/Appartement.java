e98e6ade2cad04f0d187c992f851d74c26375b11
ayoub <ayoubghoudanos@gmail.com>
2022-05-12 23:51:55 +0100

    fix after rebase

--- src/main/java/com/syndicg5/model/Appartement.java
+++ src/main/java/com/syndicg5/model/Appartement.java
@@ -29,12 +29,6 @@ public class Appartement implements Serializable {
     @Column(name = "fin")
     private Date fin;
 
-    @Column(name = "debut")
-    private Date debut;
-
-    @Column(name = "fin")
-    private Date fin;
-
     @ManyToOne
     private Resident resident;
 
